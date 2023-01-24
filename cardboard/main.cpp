76bb4d0bd7c5893706bd6eb6282dfed10fc41d57
katrmartin <katrmartin14@gmail.com>
2023-01-24 11:54:06 -0700

    working on it

---
+++ cardboard/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    int money;
+    cin << number;
+    
+    if (number <= 1) {
+        money = number * 6;
+    }
+    else {
+        number
+    }
+    
+    return 0;
+}
+
+//odd numbers don't stack evenly
+//even numbers do stack evenly
