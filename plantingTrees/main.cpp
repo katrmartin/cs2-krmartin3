40b1de025cbeb1cb60a43a221a59d64f653ea4e4
katrmartin <katrmartin14@gmail.com>
2023-02-12 18:14:04 -0700

    yep

--- plantingTrees/main.cpp
+++ plantingTrees/main.cpp
@@ -27,47 +27,33 @@ int main() {
     for (int j = 0; j < N; j++) {
         cin >> i;
         seeds.push_back(i);
-        //cout << "here" << endl;
-        if (j > N) break;
-    }
-    //cout << "Here pls" << endl;
-
-    //temp = seeds[0];
-    sort(seeds.begin(), seeds.end());
-
-    bool b = true;
-    while (b == true) {
-            for (p = 1; p < seeds.size(); p++) {
-                if (seeds[p] == seeds[p-1])
-                    seeds[p]++;   
-                     
-                
-                if (seeds[p] != seeds[p])
-                    b = false;
-                else b = true;
-            }
-            
-            
-
     }
 
-    
     sort(seeds.begin(), seeds.end(), greater<int>()); 
+    bool done = false;
+    while (!done) {
+        done = true;
+        temp = seeds[0];
+            for (p = 1; p < seeds.size(); p++) {
+                if (seeds[p] == temp) {
+                    seeds[p-1]++;   
+                    done = false;
+                }
+                else temp = seeds[p];
+                    
+            } 
+
+    }
+
+    for (auto c: seeds) {
+        cout << c << " ";
+    }
+    cout << endl;
 
     //make sure no numbers are the same, increment by 1 if they are until all numbers are different #FIXED
     //add 2 to the biggest number #FIXED
 
-    /*for (auto c: seeds) {
-        cout << c << " ";
-    }
-    */
-    //test
-    cout << endl;
-
     cout << seeds[0] + 2 << endl;
-
-
-
 
 
     return 0;
